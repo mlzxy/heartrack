3b07d6a709c0a9650108953dd76497e7323bf108
Xinyu <bevis@mail.ustc.edu.cn>
2014-12-11 21:46:52 +0800

    finish implement the utility function for the new rules

--- main/estimate_next.m
+++ main/estimate_next.m
@@ -1,25 +1,20 @@
 function [ hr ] = estimate_next( peak,prev_hr,bound )
-%UNTITLED6 Summary of this function goes here
-%   Detailed explanation goes here
-%%%%%
-%%%only use the peaks when r == max
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 persistent count stillcount;
 if isempty(count)
     count = 1;
-    stillcount = 0;
+    stillcount = 1;
 end
 count = count +1;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 
-peakx = cell(2,3);
+peaktc = cell(2,3);
 
 hb = prev_hr + bound;
 lb = prev_hr - bound;
 
-fset = [];
 for i=1:size(peak,1)
     for j=1:size(peak,2)
         peaki = peak{i,j}.peaki;
@@ -27,37 +22,29 @@ for i=1:size(peak,1)
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         peakm = peakm(peaki >= lb & peaki <= hb);
         peaki = peaki(peaki >= lb & peaki <= hb);
-        fset = [fset,peaki];
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        peakx{i,j}.peaki = peaki;
-        peakx{i,j}.peakm = peakm;
+        peaktc{i,j}.peaki = peaki;
+        peaktc{i,j}.peakm = peakm;
     end
 end
 
-% freqcell = cell(1,3);
-% for i=1:3
-%    freqcell{i} = magDecision(peakx(:,i));
-% end
-freqM = magDecision(peakx);
-freqC = [];
-magC = [];
-for i = 1:3
-    feqpekC = closeDecision(peakx(:,i),prev_hr);
-end
-freqW = wpDecision(peak);
+%%% peaktc is the truncation of peak
+freqM = magDecision(peaktc);
+[freqC,magC,distC] = closeDecision(peak,prev_hr);
+[freqW1, freqW2]= wpDecision_dual(peak);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-if isempty(freqD) 
+if isempty(freqM) 
     hr = prev_hr;    
     return;
 end
 
 %%%mixin
-if ismember(prev_hr,freqD)
-    hr = freqD(1)*0.8 + prev_hr*0.2;
+if ismember(prev_hr,freqM)
+    hr = freqM(1)*0.8 + prev_hr*0.2;
 else
-    hr = freqD(1);
+    hr = freqM(1);
 end
 
 end
